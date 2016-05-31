8b54108dcf1ada8e2047a26867f175e9d218e194
GanYi <283925583@qq.com>
2016-05-31 22:09:08 +0800

    Initial Commit

---
+++ ForOne_Client/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ForOne_Client
+//
+//  Created by GAN-mac on 16/5/31.
+//  Copyright © 2016年 gan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
